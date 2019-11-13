b6e00de635bcef4a515fc19c6e1b4cad80fb44f6
SoluiNet <16038766+SoluiNet@users.noreply.github.com>
2019-11-14 00:58:40 +0100

    SoluiNet.Doppelkopf.* - initial commit

---
+++ SoluiNet.Doppelkopf.MobileAppService/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoluiNet.Doppelkopf.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ SoluiNet.Doppelkopf/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SoluiNet.Doppelkopf.Models;
+
+namespace SoluiNet.Doppelkopf.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
